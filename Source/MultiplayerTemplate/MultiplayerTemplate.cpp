d887d4a7ffa8aa6a50956e5874012800325ce242
COLE-DESKTOP\colew <cole.wilshire@outlook.com>
2022-12-18 00:51:47 -0600

    Top-Down starter content

---
+++ Source/MultiplayerTemplate/MultiplayerTemplate.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MultiplayerTemplate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiplayerTemplate, "MultiplayerTemplate" );
+
+DEFINE_LOG_CATEGORY(LogMultiplayerTemplate)
+ 
